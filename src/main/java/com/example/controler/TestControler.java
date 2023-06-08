b6d9fdf330ca8b22bd4ce28ad26f1c6e31b2cf53
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-06-08 14:46:41 +0300

    Task 2 complete: add Thymeleaf library
    URL is http://localhost:8080/test

--- src/main/java/com/example/controler/TestControler.java
+++ src/main/java/com/example/controler/TestControler.java
@@ -3,17 +3,19 @@ package com.example.controler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDateTime;
 
 @Controller
 public class TestControler {
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    @ResponseBody
-    public String testMethod() {
-        return "This is a test method.";
+
+    @RequestMapping(value = "/test", method = {RequestMethod.GET})
+    public ModelAndView getText() {
+        ModelAndView result = new ModelAndView("test");
+        result.addObject("text", "Hello world!This is Spring MVS + Thymeleaf");
+        return result;
     }
+
+
 
 }
