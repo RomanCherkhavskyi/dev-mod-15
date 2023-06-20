1959ced42d05e19176ac4883147f736eddcc9e0a
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-06-20 10:49:39 +0300

    Commit 10: add NoteRepository.java

--- src/main/java/com/example/entity/Note.java
+++ src/main/java/com/example/entity/Note.java
@@ -5,16 +5,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "note")
 public class Note {
 
+    @Id                         //may be wrong javax or spring data?
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private long id;
 
+    @Column(name = "title", length = 100)
     private String title;
 
+    @Column(name = "content", length = 1000)
     private String content;
 
 }
--- src/main/java/com/example/services/NoteService.java
+++ src/main/java/com/example/services/NoteService.java
@@ -1,18 +1,30 @@
 package com.example.services;
 
 import com.example.entity.Note;
+import com.example.repository.NoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@RequiredArgsConstructor
 @Service
-public class NoteService {
+public class NoteService  {
 
+    private final NoteRepository repository;
     private final Map<Long, Note> notes = new HashMap<>();
+
+
+
+
     public Note add(Note note) {
+        repository.save(note);
+        System.out.println(note);
         long id = generateUniqueId();
         note.setId(id);
         notes.put(id, note);
+
         return note;
     }
 
@@ -38,6 +50,13 @@ public class NoteService {
         }else{
             notes.put(note.getId(), note);
         }
+//        repository.findById(note.getId())
+//                .stream()
+//                .findAny()
+//                .orElseThrow(() -> {
+//                    throw new NoSuchElementException("The note is missing.");
+//                });
+//        repository.save(note);
     }
     public List<Note> listAll(){
         return new ArrayList<>(notes.values());
