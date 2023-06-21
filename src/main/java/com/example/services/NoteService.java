f292db1111a6475697f21cf77c2496ef3fc83f62
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-06-21 13:34:22 +0300

    Commit 11: complete

--- src/main/java/com/example/services/NoteService.java
+++ src/main/java/com/example/services/NoteService.java
@@ -3,7 +3,6 @@ package com.example.services;
 import com.example.entity.Note;
 import com.example.repository.NoteRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -13,58 +12,42 @@ import java.util.*;
 public class NoteService  {
 
     private final NoteRepository repository;
-    private final Map<Long, Note> notes = new HashMap<>();
-
-
-
 
     public Note add(Note note) {
         repository.save(note);
-        System.out.println(note);
-        long id = generateUniqueId();
-        note.setId(id);
-        notes.put(id, note);
-
         return note;
     }
 
     public Note getById(long id) {
-        if(!notes.containsKey(id)){
-            throw new NoSuchElementException("Note with id " + id + " does not exist.");
-        }else{
-            return notes.get(id);
-        }
+                 return repository.findById(id)
+                .stream()
+                .findAny()
+                .orElseThrow(() -> {
+                    throw new NoSuchElementException("The note is missing.");
+                });
     }
 
     public void deleteById(long id)  {
-        if(!notes.containsKey(id)){
-            throw new NoSuchElementException("Note with id " + id + " does not exist.");
-        }else{
-            notes.remove(id);
-        }
+        repository.findById(id)
+                .stream()
+                .findAny()
+                .orElseThrow(() -> {
+                    throw new NoSuchElementException("The note is missing.");
+                });
+        repository.deleteById(id);
     }
 
     public void update(Note note) {
-        if(!notes.containsKey(note.getId())){
-            throw new NoSuchElementException("Note with id " + note.getId() + " does not exist.");
-        }else{
-            notes.put(note.getId(), note);
-        }
-//        repository.findById(note.getId())
-//                .stream()
-//                .findAny()
-//                .orElseThrow(() -> {
-//                    throw new NoSuchElementException("The note is missing.");
-//                });
-//        repository.save(note);
+        repository.findById(note.getId())
+                .stream()
+                .findAny()
+                .orElseThrow(() -> {
+                    throw new NoSuchElementException("The note is missing.");
+                });
+        repository.save(note);
     }
     public List<Note> listAll(){
-        return new ArrayList<>(notes.values());
+        return repository.findAll();
     }
-
-    private long generateUniqueId(){
-        return new Random().nextLong();
-    }
-
 
 }
